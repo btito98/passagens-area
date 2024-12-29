59cdae6926095602cb2483ce52d19ea7e847740f
Bruno Tito <bruno.tito@suporterenova.onmicrosoft.com>
2024-12-29 19:25:20 -0300

    feat: consumindo api para exibição das promoções

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PromocaoService } from '../../core/services/promocao.service';
+import { Promocao } from '../../core/types/types';
+import { CardBuscaComponent } from "../../shared/card-busca/card-busca.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  promocoes: Promocao[] = [];
+
+  constructor(private promocaoService: PromocaoService) {}
+
+  ngOnInit(): void {
+    this.promocaoService.listar().subscribe((resposta) => {
+      this.promocoes = resposta;
+    });
+  }
 }
