5e959c8349da29bb46628ffd8d632a4c050a6ac2
Bruno Tito <bruno.tito@suporterenova.onmicrosoft.com>
2024-12-30 23:40:15 -0300

    feat: criando tela de login

--- src/app/core/services/promocao.service.ts
+++ src/app/core/services/promocao.service.ts
@@ -12,10 +12,10 @@ export class PromocaoService {
   private apiUrl: string = environment.apiUrl;
 
   constructor(
-    private httpClient: HttpClient
+    private http: HttpClient
   ) { }
 
   listar() : Observable<Promocao[]> {
-    return this.httpClient.get<Promocao[]>(`${this.apiUrl}/promocoes`);
+    return this.http.get<Promocao[]>(`${this.apiUrl}/promocoes`);
   }
 }
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../../core/services/login.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private loginService: LoginService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      senha: [null, Validators.required],
+    });
+  }
+
+  login() {
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    let objLogin = this.loginForm.value;
+    this.loginService.autenticar(objLogin).subscribe(
+      {
+        next: (response) => {
+          console.log(response)
+          this.router.navigateByUrl('/');
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      }
+    );
+  }
+}
